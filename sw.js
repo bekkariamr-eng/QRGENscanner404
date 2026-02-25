e7b59ef8db0d369487d5684e7bf027739cf719cf
bekkariamr-eng <bekkari.amr@gmail.com>
2026-02-25 01:08:02 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-const CACHE_NAME = 'ticketqr-v2';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
